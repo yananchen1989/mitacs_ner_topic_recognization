fac9df8718feef66c34809483058a710ebf16985
DOOGLAK <douglasbowen59@gmail.com>
2022-02-06 01:22:44 -0500

    Code Updates + Testing CSVs

--- datasets/ArXiv/ArXiv API.py
+++ datasets/ArXiv/ArXiv API.py
@@ -15,13 +15,14 @@
 
 #!pip install feedparser
 
+# General Packages
 import pandas as pd
 import numpy as np
 
+# API Required/Suggested
 import urllib
 import time
 import feedparser
-from IPython.display import display
 
 # For Clearing Terminal
 import os
@@ -31,57 +32,86 @@ os.system("cls")
 ########## Calling from API ##########
 ######################################
 
+# Setup Search Terms / Categories / Other Restrictions
+search_terms = ['super%20conducting']
+# search_terms = ['superconducting','super%20conducting','super+conducting',
+#                 '"superconducting"','"super%20conducting"','"super+conducting"',
+#                 'superconduct','super%20conduct','super+conduct',
+#                 '"superconduct"','"super%20conduct"','"super+conduct"']
+                
+categories = ['cat:quant-ph']
+
+#%28all:"violet"+AND+cat:quant-ph%29&start=0&max_results=10
+#all: may not be necessary
+
 # Base API Query URL
-base_url = 'http://export.arxiv.org/api/query?'
+base_url = 'http://export.arxiv.org/api/query?search_query='
 
 # Search Parameters
-search_query = 'all:quantum%20computing'    # search for articles relating to quantum computing
+filter_cat = False                       # True or False --> Fix Query to ArXiv Categories (True = search specific categories)
 start = 0                                   # start at the first result
-total_results = 4                          # want # total results (suggested was 20)
-results_per_iteration = 2                   # Number of results at a time (suggested was 5)
-wait_time = 1                               # number of seconds to wait between calls (suggested was 3)
+total_results = 1600                          # want # total results (suggested was 20)
+results_per_iteration = 20                   # Number of results at a time (suggested was 5)
+wait_time = 3                               # number of seconds to wait between calls (suggested was 3)
 
 #Pre-Set
 Cnt=0
 API_Dict={}
 
-print('Searching arXiv for: "%s" ' % search_query)
+for search_term in search_terms:
+    search_query = 'all:%s' % (search_term)    # search for articles relating to quantum computing
 
+    print('Searching arXiv for: "%s" ' % search_query)
 
-#Looping Through Results
-for i in range(start,total_results,results_per_iteration):    
-    
-    #Output Result #s
-    print("Results %i - %i" % (i,i+results_per_iteration))
-    
-    #Query Term Using Inputs
-    query = 'search_query=%s&start=%i&max_results=%i' % (search_query, i, results_per_iteration)
-    
-    #Request from ArXiv Using URL+Query to form request
-    print('%s' % base_url+query)
-    response = urllib.request.urlopen(base_url+query).read()
-    
-    #Parse the response using feedparser
-    feed = feedparser.parse(response)
+    if filter_cat == True:
+        filter = '+AND+%s' % (categories[0])
+    else:
+        filter = ''
 
-    # Run through each entry, and grab selected information
-    for entry in feed.entries:
-        #Enter Values into Dictionary
-        API_Dict[Cnt] = [entry.id, entry.title, entry.published[:10], entry.author, entry.summary]
-        Cnt += 1
+    #Looping Through Results
+    for i in range(start,total_results,results_per_iteration):    
+        
+        #Output Result #s
+        print("Results %i - %i" % (i,i+results_per_iteration))
+        
+        #Query Term Using Inputs
+        results = '%%29&start=%i&max_results=%i' % (i, results_per_iteration)
+        query = '%%28%s' % (search_query+filter+results)
 
-    #Wait to re-call from API
-    print('Wait %i Second(s)' % wait_time)
-    time.sleep(wait_time)
-    
+        #Request from ArXiv Using URL+Query to form request
+        print('%s' % base_url+query)
+        response = urllib.request.urlopen(base_url+query).read()
+        
+        #Parse the response using feedparser
+        feed = feedparser.parse(response)
+
+        #print(feed.entries)
+        #if feed.entries == []: break # --> Exit feed if no entries - need to figure out correct method this does not work currently
+
+        # Run through each entry, and grab selected information
+        for entry in feed.entries:
+            #Enter Values into Dictionary
+            API_Dict[Cnt] = [entry.id, entry.title, entry.published[:10], entry.author, entry.summary, entry.category, categories[0], search_query]
+            Cnt += 1
+
+        #Wait to re-call from API
+        print('Wait %i Second(s)' % wait_time)
+        time.sleep(wait_time)
+
 # Exited For Loop
 print("Data Gathering Complete")
 
 # Generate Dataframe, Make Edits, Sort, Output to CSV
-API_Output = pd.DataFrame.from_dict(API_Dict,orient='index', columns = ["URL", "Title", "Published", "Author", "Abstract"])
+API_Output = pd.DataFrame.from_dict(API_Dict,orient='index', columns = ["URL", "Title", "Published", "Author", "Abstract", "Atom Category", "Filtered Category", "Search Term"])
 API_Output["Published"] = pd.to_datetime(API_Output["Published"])
 API_Output = API_Output.sort_values(by="Published", ascending=False)
 
-API_Output.to_csv('nlp4quantumpapers/datasets/ArXiv/API_Output.csv',index=False)
+if filter_cat == True: CSV_Path = 'nlp4quantumpapers/datasets/ArXiv/API_Output_CatFilterTrue_Testing.csv'
+if filter_cat == False: CSV_Path = 'nlp4quantumpapers/datasets/ArXiv/API_Output_CatFilterFalse_Testing.csv'
+
+#Temporarily Make General Single-Search Term Output
+CSV_Path = 'nlp4quantumpapers/datasets/ArXiv/API_Output.csv'
+
+API_Output.to_csv(CSV_Path,index=False)
 print("Output to CSV Complete")
 
