e475bf95d53adf788e294546c822b8d8cd37c480
yanan <yanan@yanandeMacBook-Air.local>
2022-03-20 15:21:55 -0400

    fix

--- train_fasttext_cate.py
+++ train_fasttext_cate.py
@@ -1,8 +1,8 @@
 import fasttext
 for ep in [7, 12, 25, 30, 50, 70, 100]:
-    model = fasttext.train_supervised(input="./abstract_cate.train", dim=128,\
-             loss='softmax', thread=128,  minn=1, maxn=5, wordNgrams=4, epoch=ep)
+    model = fasttext.train_supervised(input="/home/w/wluyliu/yananc/nlp4quantumpapers/abstract_cate.train", dim=128,\
+             loss='softmax', thread=48,  minn=1, maxn=5, wordNgrams=4, epoch=ep)
     
-    result = model.test("abstract_cate.test")
+    result = model.test("/home/w/wluyliu/yananc/nlp4quantumpapers/abstract_cate.test")
     print("epoch:{}".format(ep), result)
-    model.save_model("./abstract_cate_ep{}.bin".format(ep))
+    model.save_model("/scratch/w/wluyliu/yananc/abstract_cate_ep{}.bin".format(ep))
