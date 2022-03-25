66d4cf37d6920da5c1ed8e7058d250d0420a2025
yanan <yanan@yanandeMacBook-Air.local>
2022-03-25 11:59:09 -0400

    fix

--- topic/bert_topic.py
+++ topic/bert_topic.py
@@ -1,39 +1,39 @@
-import json
-
-# https://arxiv.org/help/api/user-manual
-
-
 import pandas as pd 
+from utils.process_func import * 
+
+df = pd.read_csv("/home/w/wluyliu/yananc/nlp4quantumpapers/artificially_labeled_abstracts.csv")
+dfi = df.loc[df['Assigned_group']=='Full stack and quantum computers']
+dfi['abstract_clean'] = dfi['abstract'].map(lambda x: remove_latex(x))
+dfi['abstract_stem'] = dfi['abstract_clean'].map(lambda x: clean_title(x))
+
+
 from bertopic import BERTopic
 
+######## load model 
 from sentence_transformers import SentenceTransformer
-#embedding_model = SentenceTransformer("all-mpnet-base-v2", device='cpu', cache_folder='./cache_sentbert')
-embedding_model = SentenceTransformer("/Users/yanan/Downloads/finetune/arxiv_scibert_quantph", device='cpu')
-embeddings = model.encode(df.sample(2048)['abstract'].tolist(), show_progress_bar=True, batch_size=64)
+embedding_model = SentenceTransformer("all-mpnet-base-v2", device='cuda', cache_folder='./cache_sentbert')
+#embedding_model = SentenceTransformer("/Users/yanan/Downloads/finetune/arxiv_scibert_quantph", device='cpu')
+
+for min_topic_size in [16, 32, 64, 128, 256]:
+
+    topic_model = BERTopic(embedding_model=embedding_model, verbose=True, min_topic_size=min_topic_size)
+    topics, probs = topic_model.fit_transform(dfi['abstract_stem'].tolist())
+    print("min_topic_size:{}".format(min_topic_size) )
+    print("number of topics:{}".format(len(topic_model.get_topic_info())))
+
+    for i in range(len(topic_model.get_topic_info())):
+        print("topic==>{}".format(i)) 
+        for ii in topic_model.get_topic(i):
+            print(ii[0], round(ii[1],4) )    
+        print()
 
 
 
-topic_model = BERTopic(embedding_model=embedding_model, verbose=True, min_topic_size=50)
-topics, probs = topic_model.fit_transform(df['abstract'].tolist())
 
+'''
 topic_model.save("mpnet_topic_model_sci")
-
-
-
 topic_model = BERTopic.load("mpnet_topic_model_sci")
 
-len(topic_model.get_topic_info())
-
-topic_model.get_topic_info().head(10)
-
-topic_model.get_topic(-1)
-
-
-
-for ix, row in topic_model.get_topic_info(8).iterrows():
-    print(row['Topic'], row['Count'], row['Name'])
-    topic_model.get_topic(row['Topic'])
-    print()
 
 
 
@@ -56,11 +56,91 @@ topics_over_time = topic_model.topics_over_time(df['abstract'].tolist(), topics,
 topic_model.visualize_topics_over_time(topics_over_time, top_n_topics=len(topic_model.get_topic_info()), \
                 width=900, height=500, normalize_frequency=True)
 
+'''
+
+'''
+
+################################# implement from scratch ################################# 
+
+embeddings = embedding_model.encode(dfi['abstract_stem'].tolist(), show_progress_bar=True, batch_size=512)
+
+
+import umap,hdbscan
+umap_embeddings = umap.UMAP(n_neighbors=15, 
+                            n_components=5, 
+                            metric='cosine').fit_transform(embeddings)
+
+cluster = hdbscan.HDBSCAN(min_cluster_size=5,
+                          metric='euclidean',                      
+                          cluster_selection_method='eom').fit(umap_embeddings)
+
+dfs['label'] = cluster.labels_
+
+print("pred label:\n", dfs.label.value_counts())  
+
+
+# result = pd.DataFrame(umap_data, columns=['x', 'y'])
+# result['label'] = cluster.labels_
+
+# # Visualize clusters
+# fig, ax = plt.subplots(figsize=(20, 10))
+# outliers = result.loc[result.labels == -1, :]
+# clustered = result.loc[result.labels != -1, :]
+# plt.scatter(outliers.x, outliers.y, color='#BDBDBD', s=0.05)
+# plt.scatter(clustered.x, clustered.y, c=clustered.labels, s=0.05, cmap='hsv_r')
+# plt.colorbar()
+
+# docs_df = pd.DataFrame(data, columns=["Doc"])
+# docs_df['Topic'] = cluster.labels_
+# docs_df['Doc_ID'] = range(len(docs_df))
+# docs_per_topic = docs_df.groupby(['Topic'], as_index = False).agg({'Doc': ' '.join})
 
 
 
+docs_per_topic = dfs.groupby(['label'], as_index = False).agg({'abstract': ' '.join})
 
 
+
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+def c_tf_idf(documents, m, ngram_range=(1, 1)):
+    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+    t = count.transform(documents).toarray()
+    w = t.sum(axis=1)
+    tf = np.divide(t.T, w)
+    sum_t = t.sum(axis=0)
+    idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+    tf_idf = np.multiply(tf, idf)
+
+    return tf_idf, count
+  
+tf_idf, count = c_tf_idf(docs_per_topic['abstract'].values, m=dfs.shape[0], ngram_range=(1,3))
+
+def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+    words = count.get_feature_names()
+    labels = list(docs_per_topic['label'])
+    tf_idf_transposed = tf_idf.T
+    indices = tf_idf_transposed.argsort()[:, -n:]
+    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    return top_n_words
+
+def extract_topic_sizes(df):
+    topic_sizes = (df.groupby(['label'])
+                     .abstract
+                     .count()
+                     .reset_index()
+                     .rename({"label": "label", "abstract": "Size"}, axis='columns')
+                     .sort_values("Size", ascending=False))
+    return topic_sizes
+
+top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20)
+topic_sizes = extract_topic_sizes(dfs)
+
+print("topic_sizes==>\n", topic_sizes)
+
+for l, kws in top_n_words.items():
+    print(l, ' '.join([j[0] for j in kws]))
 
 
 
