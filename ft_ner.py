130b599485955b7500f981ad95655fd44534e725
yanan <yanan@yanandeMacBook-Air.local>
2022-04-18 18:40:18 -0400

    fix

--- ft_ner.py
+++ ft_ner.py
@@ -380,7 +380,7 @@ def train(epoch):
         
         if idx % 128 == 0:
             loss_step = tr_loss/nb_tr_steps
-            print(f"Training loss per 100 training steps: {loss_step}")
+            print(f"Training loss per 128 training steps: {loss_step}")
            
         # compute training accuracy
         flattened_targets = labels.view(-1) # shape (batch_size * seq_len,)
@@ -519,41 +519,49 @@ Here, we use the prediction of the **first word piece of every word** (which is 
 that belong to the same word do not match.*
 """
 
-sentence = "@HuggingFace is a company based in New York, but is also has employees working in Paris"
 
-inputs = tokenizer(sentence.split(),
-                    # is_pretokenized=True, 
-                    is_split_into_words=True, 
-                    return_offsets_mapping=True, 
-                    padding='max_length', 
-                    truncation=True, 
-                    max_length=MAX_LEN,
-                    return_tensors="pt")
 
-# move to gpu
-ids = inputs["input_ids"].to(device)
-mask = inputs["attention_mask"].to(device)
-# forward pass
-outputs = model(ids, attention_mask=mask)
-logits = outputs[0]
+def infer(sentence, model):
+    inputs = tokenizer(sentence.split(),
+                        # is_pretokenized=True, 
+                        is_split_into_words=True, 
+                        return_offsets_mapping=True, 
+                        padding='max_length', 
+                        truncation=True, 
+                        max_length=MAX_LEN,
+                        return_tensors="pt")
 
-active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
-flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
+    # move to gpu
+    ids = inputs["input_ids"].to(device)
+    mask = inputs["attention_mask"].to(device)
+    # forward pass
+    outputs = model(ids, attention_mask=mask)
+    logits = outputs[0]
 
-tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
-token_predictions = [ids_to_labels[i] for i in flattened_predictions.cpu().numpy()]
-wp_preds = list(zip(tokens, token_predictions)) # list of tuples. Each tuple = (wordpiece, prediction)
+    active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
+    flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
 
-prediction = []
-for token_pred, mapping in zip(wp_preds, inputs["offset_mapping"].squeeze().tolist()):
-  #only predictions on first word pieces are important
-  if mapping[0] == 0 and mapping[1] != 0:
-    prediction.append(token_pred[1])
-  else:
-    continue
+    tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
+    token_predictions = [ids_to_labels[i] for i in flattened_predictions.cpu().numpy()]
+    wp_preds = list(zip(tokens, token_predictions)) # list of tuples. Each tuple = (wordpiece, prediction)
 
-print(sentence.split())
-print(prediction)
+    prediction = []
+    for token_pred, mapping in zip(wp_preds, inputs["offset_mapping"].squeeze().tolist()):
+      #only predictions on first word pieces are important
+      if mapping[0] == 0 and mapping[1] != 0:
+        prediction.append(token_pred[1])
+      else:
+        continue
+
+    dfr = pd.DataFrame(zip(sentence.split(), prediction), columns=['span','tag'])
+    return dfr 
+
+
+sent = "@HuggingFace is a company based in New York, but is also has employees working in Paris"
+
+dfr = infer(sent, model)
+print(dfr)
+
 
 """#### **Saving the model for future use**
 
--- ners/transformers_ners.py
+++ ners/transformers_ners.py
@@ -5,6 +5,8 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 
+
+gpu = 0
 # conll2003
 # tokenizer_bert = AutoTokenizer.from_pretrained("dslim/bert-base-NER", cache_dir='/scratch/w/wluyliu/yananc/cache')
 # model_bert = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER", cache_dir='/scratch/w/wluyliu/yananc/cache')
@@ -12,18 +14,23 @@ from transformers import pipeline
 # conll2003
 tokenizer_roberta = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english", cache_dir='/scratch/w/wluyliu/yananc/cache')
 model_roberta = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english", cache_dir='/scratch/w/wluyliu/yananc/cache')
-
-# nlp_bert = pipeline("ner", model=model_bert, tokenizer=tokenizer_bert, aggregation_strategy="simple")
-nlp_roberta = pipeline("ner", model=model_roberta, tokenizer=tokenizer_roberta, aggregation_strategy="simple")
-
-
-path_local = "/scratch/w/wluyliu/yananc/finetunes/roberta_nerd_fine" 
-tokenizer_roberta = AutoTokenizer.from_pretrained(path_local)
-model_roberta = AutoModelForTokenClassification.from_pretrained(path_local)
+nlp_roberta = pipeline("ner", model=model_roberta, tokenizer=tokenizer_roberta, \
+                    aggregation_strategy="simple", device=gpu)
 
 
+tokenizer_roberta_nerd_fine = AutoTokenizer.from_pretrained("/scratch/w/wluyliu/yananc/finetunes/roberta_nerd_fine")
+model_roberta_nerd_fine = AutoModelForTokenClassification.from_pretrained("/scratch/w/wluyliu/yananc/finetunes/roberta_nerd_fine")
+nlp_roberta_nerd_fine = pipeline("ner", model=model_roberta_nerd_fine, tokenizer=tokenizer_roberta_nerd_fine,\
+                     aggregation_strategy="simple", device=gpu)
 
-def check_entity_ft(sent):
+
+tokenizer_roberta_nerd_coarse = AutoTokenizer.from_pretrained("/scratch/w/wluyliu/yananc/finetunes/roberta_nerd_coarse")
+model_roberta_nerd_coarse = AutoModelForTokenClassification.from_pretrained("/scratch/w/wluyliu/yananc/finetunes/roberta_nerd_coarse")
+nlp_roberta_nerd_coarse = pipeline("ner", model=model_roberta_nerd_coarse, tokenizer=tokenizer_roberta_nerd_coarse,\
+                    aggregation_strategy="simple", device=gpu)
+
+
+def check_entity_ft(sent, nlp_roberta, fmark):
     ner_results = nlp_roberta(sent)
     if not ner_results:
         return []
@@ -31,7 +38,7 @@ def check_entity_ft(sent):
     for ii in ner_results:
         if ii['word'].strip() in string.punctuation:
             continue
-        infos.append((ii['entity_group'], ii['word'].strip()))
+        infos.append((ii['entity_group'], ii['word'].strip(), fmark))
     return infos
 
 
@@ -48,14 +55,16 @@ nlp_split_sents = spacy.load('en_core_web_sm')
 from flair.data import Sentence
 from flair.models import SequenceTagger
 import torch, flair, string
-#flair.device = torch.device('cpu')
-flair.device = torch.device('cuda:0')
-#tagger = SequenceTagger.load("flair/ner-english-fast")
-# https://github.com/flairNLP/flair/blob/master/flair/data.py
+
+if gpu >= 0:
+    flair.device = torch.device('cuda:{}'.format(gpu))
+else:
+    flair.device = torch.device('cpu')
+
 tagger_conll = SequenceTagger.load("flair/ner-english-large")
 tagger_ontonotes = SequenceTagger.load("flair/ner-english-ontonotes-large")
 
-def check_entity_flair(content, tagger):
+def check_entity_flair(content, tagger, fmark):
     sentence = Sentence(content)
     tagger.predict(sentence)
     # ii.start_position, ii.end_position
@@ -66,7 +75,7 @@ def check_entity_flair(content, tagger):
             continue
         if ii.text.strip() in string.punctuation:
             continue
-        infos.append((ii.tag, ii.text.strip()))
+        infos.append((ii.tag, ii.text.strip(), fmark))
     return infos
 
 
@@ -78,47 +87,71 @@ def check_entity_flair(content, tagger):
 with open('/home/w/wluyliu/yananc/nlp4quantumpapers/articles_full.json', 'r') as f:
     jxml = json.load(f)
 
-random.shuffle(jxml)
+tag_map = {'PERSON':'PER', 'ORGANIZATION':'PER', 'LOCATION':'LOC', 'GPE':'LOC'}
+pick_from_nerd = ['EDUCATION','COMPANY','SOFTWARE','Government'.upper() ]
+
+def ner_tag(content):
+    sents = [i.text.replace('\n','').strip() for i in nlp_split_sents(content).sents]
+
+    infos = []
+    for sent in sents:
+        if not sent:
+            continue
+        flair_ontonotes_ner_results = check_entity_flair(sent, tagger_ontonotes, 'flair_onto')
+        infos.extend(flair_ontonotes_ner_results)
+
+        flair_conll_ner_results = check_entity_flair(sent, tagger_conll, 'flair_conll')
+        infos.extend(flair_conll_ner_results)
+
+        ft_ner_ori = check_entity_ft(sent, nlp_roberta, 'ft_ori')
+        infos.extend(ft_ner_ori)
+
+        ft_ner_nerd_coarse = check_entity_ft(sent, nlp_roberta_nerd_coarse, 'nerd_coarse') 
+        infos.extend(ft_ner_nerd_coarse)
+
+        ft_ner_nerd_fine = check_entity_ft(sent, nlp_roberta_nerd_fine, 'nerd_fine') 
+        infos.extend(ft_ner_nerd_fine)
+
+        print(sent) 
+
+    
+    df = pd.DataFrame(infos, columns=['ner','span', 'fmark'])
+    df['ner'] = df['ner'].map(lambda x: x.upper())
+    df['ner'] = df['ner'].map(lambda x: tag_map[x] if x in tag_map else x )
+
+    df = df.loc[df['ner'].str()]
+    df.drop_duplicates(['ner','span', 'fmark'], inplace=True)
+
+    df_pretrain = df.loc[df['fmark'].isin(['flair_onto', 'flair_conll', 'ft_ori'])]
+    df_nerd = df.loc[df['fmark'].isin(['nerd_coarse', 'nerd_fine'])]
+    df_nerd = df_nerd.loc[df_nerd['ner'].isin(pick_from_nerd)]
+
+    df_pretrain.drop_duplicates(['ner','span'], inplace=True)
+    df_nerd.drop_duplicates(['ner','span'], inplace=True)
+    
+
+    df_pretrain.sort_values(by=['ner'], ascending=True, inplace=True)
+    df_nerd.sort_values(by=['ner'], ascending=True, inplace=True)
+    return df_pretrain, df_nerd
+
 content = random.sample(jxml, 1)[0]['post_content']
+df_pretrain, df_nerd = ner_tag(content)
 
 
-
-sents = [i.text for i in nlp_split_sents(content).sents]
-
-
-infos = []
-for sent in sents:
-    ft_ner_results = check_entity_ft(sent)
-    infos.extend(ft_ner_results)
-    print(sent) 
-
-
-infos = []
-for sent in sents:
-    flair_ontonotes_ner_results = check_entity_flair(sent, tagger_ontonotes)
-    flair_conll_ner_results = check_entity_flair(sent, tagger_conll)
-    ft_ner_results = check_entity_ft(sent)
-    infos.extend(flair_ontonotes_ner_results)
-    infos.extend(flair_conll_ner_results)
-    infos.extend(ft_ner_results)
-    print(sent) 
-
-df = pd.DataFrame(infos, columns=['ner','span'])
-df['ner'] = df['ner'].map(lambda x: 'PER' if x=='PERSON' else x )
-df.drop_duplicates(['ner','span'], inplace=True)
-df.sort_values(['ner', 'span'], inplace=True)
-
-for ix, row in df.iterrows():
+for ix, row in df_pretrain.iterrows():
     print(row['ner'], '\t', row['span'])
+
+
+print()
+for ix, row in df_nerd.iterrows():
+    print(row['ner'], '\t', row['span'])
+
 
 
 labels_candidates = ['company', 'investor', 'university','Venture Capital Investor', 'academic organization', 
                         'goverment organization']
 
-for span in df.loc[df['ner']=='ORG']['span'].tolist():
-    result_nli = nli_nlp_roberta(content, labels_candidates, multi_label=True, hypothesis_template= span + " is a {}.")
-    dfr = pd.DataFrame(zip(result_nli['labels'], result_nli['scores']) , columns=['cate','score'])
-    print(span, '\n', dfr)
+
 
 
 
