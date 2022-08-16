e5b95eedab5993938e1388c69ee9080e3ba617ee
Herteg Kohar <55204675+koha5160@users.noreply.github.com>
2022-08-16 11:14:08 -0400

    Update export_prodigy.bash

--- prodigy_shell_scripts/export_prodigy.bash
+++ prodigy_shell_scripts/export_prodigy.bash
@@ -1,2 +1,2 @@
 #python3 -m prodigy db-out {database name} > {output filename (jsonl)}
-python3 -m prodigy db-out tmp > ./prodigy_annotations.jsonl
+python3 -m prodigy db-out tmp > ./data/prodigy_annotations.jsonl
