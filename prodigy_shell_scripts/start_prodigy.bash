57e1951bf40bd6a5e9a3e38ef21f3054dcd3d280
Herteg Kohar <55204675+koha5160@users.noreply.github.com>
2022-08-16 11:11:30 -0400

    Update start_prodigy.bash

--- prodigy_shell_scripts/start_prodigy.bash
+++ prodigy_shell_scripts/start_prodigy.bash
@@ -1,2 +1,2 @@
 #python3 -m prodigy <recipe> <database> <tokenizing> <article jsonl input file> --label group,university,investor,lab,gov,company
-python3 -m prodigy ner.manual tmp blank:en prodigy_articles.jsonl --label group,university,investor,lab,gov,company
+python3 -m prodigy ner.manual tmp blank:en data/prodigy_articles.jsonl --label group,university,investor,lab,gov,company
